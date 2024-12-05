b5e677ffd016b36743b8c268a135bcec7285cf97
https://github.com/MoogiWilfred <122815519+MoogiWilfred@users.noreply.github.com>
2024-12-06 02:19:59 +0300

    2nd commit

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,5 +1,26 @@
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_stars from './rating_stars.png'
+import profile_icon from './profile_icon.png'
+
+
 import header_1 from './H1.jpg'
 import header_2 from './H2.jpg'
+
+
+
+export const assets={
+  add_icon_white,
+  add_icon_green,
+  remove_icon_red,
+  cross_icon,
+  selector_icon,
+  rating_stars,
+  profile_icon
+}
 
 
 export const header_list=[
--- client/src/components/main/main.jsx
+++ client/src/components/main/main.jsx
@@ -1,12 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import './main.css';
 import axios from 'axios';
-
+import { assets } from '../../assets/assets';
 
 function Main() {
   const url = 'http://localhost:4000'; // Base URL for your backend server
 
   const [data, setData] = useState([]);
+  const [cartItem, setCartItem]=useState({})
+
+
+  const addItemToCart = (itemId) => {
+    setCartItem((prev) => ({
+    
+      ...prev,
+       [itemId]: prev[itemId]?  prev[itemId] + 1 : 1,})
+    
+    )
+  };
+
+ const  removeItemFromCart=(itemId)=>{
+    setCartItem((prev)=>{
+    if (prev[itemId]>1){
+      return {...prev, [itemId]: prev[itemId] -1}
+    }
+  else{
+      const updatedCart = { ...prev };
+        delete updatedCart[itemId];
+        return updatedCart;
+    }
+
+
+       })
+
+      
+    
+  }
+
 
   useEffect(() => {
     const getData = async () => {
@@ -30,6 +60,21 @@ function Main() {
             src={`${url}/images/${item.image}`} alt={item.name}
           
           />
+          <div className="remove-add">
+            <div className="remove">
+              <img src={assets.remove_icon_red} onClick={()=>removeItemFromCart(item._id)}/></div>
+            
+              <div className="add-white">
+
+                <img src={assets.add_icon_white} onClick={()=>addItemToCart(item._id)}/>
+
+                
+                </div>
+                <div className="add-green"><img src={assets.add_icon_green} onClick={()=>addItemToCart(item._id)}/>
+                </div>
+                <p> {cartItem[item._id]}  </p>
+                </div>
+
           <div className="info">
             <p className="left">{item.name}</p>
             <p className="right">Ksh.{item.price}</p>
