82fe0b118cc7cfb78c585735a46a0af0c296b879
https://github.com/MoogiWilfred <122815519+MoogiWilfred@users.noreply.github.com>
2024-12-13 14:57:45 +0300

    modified the header

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -6,11 +6,11 @@ function header() {
 
   return (
     <div className='header-container'>
-     <img className='popup-image-right 'src={assets.header_3} />
+    {/*} <img className='popup-image-right 'src={assets.header_3} />*/}
     <img className='popup-image 'src={assets.header_2} />
 <div className="left">
-   <img className='popup-image-left 'src={assets.header_3} />
-   <div className="ads"><h1>Welcome</h1></div>
+   {/*<img className='popup-image-left 'src={assets.header_3} />
+   <div className="ads"><h1>Welcome</h1></div>*/}
 </div>
 </div>
 
