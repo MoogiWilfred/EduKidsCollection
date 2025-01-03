b4f215bee9e881dabb98128db62adef32d877cb0
https://github.com/MoogiWilfred <122815519+MoogiWilfred@users.noreply.github.com>
2025-01-04 02:25:55 +0300

    Added advert section on the header

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,17 +1,46 @@
 import React from 'react'
 import './header.css'
-import {assets} from '../../assets/assets'
-
+import { assets } from '../../assets/assets'
 function header() {
 
   return (
     <div className='header-container'>
-    {/*} <img className='popup-image-right 'src={assets.header_3} />*/}
-    <img className='popup-image 'src={assets.header_2} />
-<div className="left">
-   {/*<img className='popup-image-left 'src={assets.header_3} />
-   <div className="ads"><h1>Welcome</h1></div>*/}
-</div>
+   <div className='popup-left' > 
+    <div className='new-shops'>
+    <h4>We are Now Near You</h4> 
+    <ul>
+      <li>Kisii: Kisii Plaza</li>
+      <li>Eldoret: The 254 Building</li>
+      <li>Kisumu: Kisumumatt</li>
+      <li>Nairobi: Nairobi GatewayMall</li>
+      <li>Kakamega:Kakamega Plaza</li>
+      <li>Muranga: The Readers' Building</li>
+      
+    </ul>
+    </div>
+    <div className='advert'>
+<p className="text-advert">
+  Free Delivery&nbsp;
+  <span >
+    <span>WithinCBD</span>
+    <span>YourFirstOrder</span>
+    <span>OrderAboveKsh.10,000</span>
+  </span>
+</p>
+
+  </div>
+    </div>
+   <img className='popup-image 'src={assets.header_2} />
+
+<div className='popup-right' >
+<div className='popup-image-right' ></div>
+
+<div className='contact'>
+<h1>CALL US:</h1> 
+<p> +254 778 188 922</p>
+
+  </div>
+  </div>
 </div>
 
   )
